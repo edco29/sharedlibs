c940c34ca010644608a3075e8b9bc92c0cac655f
edco29 <40572443+edco29@users.noreply.github.com>
2020-03-23 07:00:53 -0500

    update

--- src/sharedlibs/jenkinsfileUtil.groovy
+++ src/sharedlibs/jenkinsfileUtil.groovy
@@ -1,27 +1,12 @@
 class jenkinsfileUtil implements Serializable {
 
   def buildTimestamp
-  //def mvnHome
-  /*
-  * Get information from the main pipeline
-  */
   def steps
   def script
-  //def docker
   def type
-  jenkinsfileUtil(steps,script,String type = '') {
-    /*  def remoteConfigs=script.scm.getUserRemoteConfigs()
-      for (Object remoteConfig : remoteConfigs) {
-        currentCredentialsId= remoteConfig.getCredentialsId()
-        steps.echo "CurrentCredentialsId for ${remoteConfig} --> ${currentCredentialsId}"
-      }
-*/
+  jenkinsfileUtil(steps,script) {
       this.steps = steps
       this.script = script
-      steps.echo "steps: ${steps}"
-      steps.echo "script: ${script}"
-    //  docker=new Docker(this.script)
-      this.type=type
   }
 
   def setbuildtimestamp (){
@@ -31,19 +16,10 @@ class jenkinsfileUtil implements Serializable {
   }
 
 /*
-  * This method compile the doce from maven
+  * This method compile  maven projects
   */
-  def buildMaven(String buildParameters = '', boolean failNever=false) {
-     // Run the maven build
-     //steps.sh "'${mvnHome}/bin/mvn' compile test-compile"
-    // steps.sh "mvn compile test-compile"
-     def failNeverParam=""
-     if(failNever){
-         failNeverParam="--fail-never"
-     }
-    // steps.sh "'${mvnHome}/bin/mvn' ${failNeverParam} -DskipITs ${buildParameters} install"
-       steps.sh "mvn ${failNeverParam} -DskipITs ${buildParameters} package"
-  
+  def buildMaven(String buildParameters = '') {
+       steps.sh "mvn --fail-never -DskipITs ${buildParameters} package"
   }
   
   def deployMaven (){
